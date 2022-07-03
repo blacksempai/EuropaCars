fdbc9cd1c24662a67e32d08c0223361b3218d507
blacksempai <benjaminfranclin1998@gmail.com>
2022-07-03 22:54:57 +0300

    return resize listener

--- mobile.js
+++ mobile.js
@@ -1,8 +1,9 @@
 window.onload = setViewPort;
+window.onresize = setViewPort;
+let viewport = document.querySelector("meta[name=viewport]");
+let viewheight = window.innerHeight;
 function setViewPort() {
-    let viewheight = window.innerHeight;
     let viewwidth = window.innerWidth;
-    let viewport = document.querySelector("meta[name=viewport]");
     viewport.setAttribute("content", "height=" + viewheight + ", width=" + viewwidth + ", initial-scale=1.0, maximum-scale=1.0, user-scalable=0");
 };
 
